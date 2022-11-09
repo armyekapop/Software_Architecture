b07b31beb5b2180159cbdecc7d7aaa4f96c6209b
armyekapop <69144519+armyekapop@users.noreply.github.com>
2022-11-09 15:31:38 +0700

    63011090 Structural Design Patterns - First commit

---
+++ StructuralDesignPatterns/src/HealthcareWorkerTeam.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+
+    private List<HealthcareServiceable> members = new ArrayList<HealthcareServiceable>();
+    @Override
+    public void service() {
+        for(HealthcareServiceable item : members) {
+            item.service();
+        }
+
+    }
+
+    @Override
+    public double getPrice() {
+        int totalPrice = 0;
+        for(HealthcareServiceable item : members) {
+            totalPrice += item.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void addMember(HealthcareServiceable Member) {
+        members.add(Member);
+
+    }
+
+    public void removeMember(HealthcareServiceable Member) {
+        members.remove(Member);
+
+    }
+
+}
